fc2f3741611580b7f3b01a305a1be73e53b8c91e
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-11 13:34:08 -0400

    added styling, done, but dont like styling

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -1,54 +1,75 @@
 import React, {useEffect, useState} from "react";
 
-import {Card} from 'semantic-ui-react';
+
+
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
 import styled from "styled-components";
+import axios from 'axios';
 
 
-const Background = styled.div`
 
-display: flex;
-flex-direction: column;
-justify-content: center;
-margin: 5%;
 
+const Cardstyle = styled.div`
+border: 2px solid black;
+background: #e8d89f;
+ width: 300px;
 `;
 
 
 
 
-export default function PersonCard = () => {
+const PersonCard = () => {
 
 const [starwarInfo, setStarwarInfo] = useState([])
 
-useEffect(()=> {
+useEffect(() => {
     axios
-    .get ('http https://swapi.co/api/people')
+    .get (`https://swapi.co/api/people/`)
     .then(response => {
-        const res = response.data;
-        console.log('response', Response.data);
-        setStarwar(res);
+        const res = response.data.results;
+        console.log('response', response.data.results);
+        setStarwarInfo(res);
     });
 }, []);
 
 return(
 
-<Background>
 
-
+<div>
+    
+         
     {starwarInfo.map(props => {
         return(
-            <Card>
-            <h1>Name:</h1>
-            <h2>birth Year:</h2>
-            <h2>Height: </h2>
-            <h2>Weight: </h2>
 
-            </Card>
+        
+        <Cardstyle>
+        <Card>
+            {/* <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" /> */}
+            <CardBody>
+                <CardTitle>Name:{props.name}</CardTitle>
+                <CardSubtitle>birth Year:{props.birth_year}</CardSubtitle>
+                <CardSubtitle>Height:{props.height}</CardSubtitle>
+                <CardSubtitle>Weight:{props.mass}</CardSubtitle> 
+                {/* <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
+                {/* <Button>Button</Button> */}
+            </CardBody>
+      </Card>
+      </Cardstyle>
+
         )
+
+        
     })}
 
-</Background>
+</div>
+
 
 )
 
 }
+
+export default PersonCard
